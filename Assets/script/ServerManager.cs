21fe9218de7af422b62d1dc475c09cbfa3029b9a
layanmeta <layan.meta@gmail.com>
2022-07-16 16:32:54 +0300

    .

--- Assets/script/ServerManager.cs
+++ Assets/script/ServerManager.cs
@@ -15,7 +15,7 @@ public class Question
     public string ans2_text;
     public string ans3_text;
     public string ans4_text;
-    public int correct_ans_id;
+    private int correct_ans_id;
 }
 
 public class ServerManager : MonoBehaviour
